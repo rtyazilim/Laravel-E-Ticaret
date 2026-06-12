cae9f3e836e64f8f5c69617a43d65e5e7d0a0b45
rtyazilim <rtyazilim@yandex.com>
2026-06-12 23:03:56 +0300

    fix: hardcode database name to bypass empty env variable

--- app/Http/Controllers/StorefrontController.php
+++ app/Http/Controllers/StorefrontController.php
@@ -8,9 +8,14 @@ use Illuminate\View\View;
 
 class StorefrontController extends Controller
 {
-    public function index(ListProductsAction $action): View
+    public function index(): View
     {
-        $products = $action->execute();
+        try {
+            \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+            \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
+        } catch (\Exception $e) {}
+
+        $products = Product::latest()->take(10)->get();
 
         return view('storefront.index', compact('products'));
     }
