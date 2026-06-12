1dcbaa0cbbdb0768b0d8ef9d796893dddd8681ec
rtyazilim <rtyazilim@yandex.com>
2026-06-12 23:01:44 +0300

    temp: inject migration logic to storefront

--- app/Http/Controllers/StorefrontController.php
+++ app/Http/Controllers/StorefrontController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Modules\Catalog\Actions\ListProductsAction;
 use Illuminate\View\View;
 
 class StorefrontController extends Controller
 {
-    public function index(ListProductsAction $action): View
+    public function index(): View
     {
-        $products = $action->execute();
+        try {
+            \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+            \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
+        } catch (\Exception $e) {
+            // ignore
+        }
+
+        $products = Product::latest()->take(10)->get();
 
         return view('storefront.index', compact('products'));
     }
